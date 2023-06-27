4dde23a3e98cf05e47132b950dc8185df342dbf5
Micaeli Takara Becca <Mikaelitakara@gmail.com>
2023-06-27 15:23:23 -0300

    aula 26/06

---
+++ src/database.ts
@@ -0,0 +1,35 @@
+import { TUsers } from "./types";
+
+import { TProducts } from "./types";
+
+export const user: TUsers[] = [
+    {
+        id: "u001",
+        name: "Fulano",
+        email: "fulano@email.com",
+        password: "fulano123",
+        createdAt: new Date().toISOString()
+    }, {
+        id: "u002",
+        name: "Beltrana",
+        email: "beltrana@gmail.com",
+        password: "beltrana00",
+        createdAt: new Date().toISOString()
+    }
+]
+
+export const product: TProducts[] = [
+    {
+        id: "prod001",
+        name: "Mouse gamer", 
+        price: 250,
+        description: "Melhor mouse do mercado!",
+        imageUrl: "https://picsum.photos/seed/Mouse%20gamer/400"
+    }, {
+        id: "prod002",
+        name: "Monitor",
+        price: 900,
+        description: "Monitor LED Full HD 24 polegadas",
+        imageUrl: "https://picsum.photos/seed/Monitor/400"
+    }
+]
